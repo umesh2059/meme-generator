9434aedeb0670622ab1cfe346ab9b4a82d09a908
Umesh gupta <umeshgupta67867@gmmail.com>
2025-04-04 23:28:49 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{ useState } from 'react';
+import{Routes,Route} from "react-router-dom";
+import HomePage from "./pages/Home"
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
 
 function App() {
   return(
-    <div>
+    <div className='Container'>
       <h1>meme generator</h1>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+      </Routes>
+
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MemeCard from "./components/card";
+const Homepage=()=>{
+    return(
+   <div>
+    <MemeCard/>
+   </div>        
+    );
+};
+export default Homepage;
