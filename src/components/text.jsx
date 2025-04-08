621b4c46abee35646ed217ff28a7905a2e244a5c
Umesh gupta <umeshg2059@gmail.com>
2025-04-08 09:17:03 +0530

    features

--- src/components/text.jsx
+++ src/components/text.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-import Draggable from "react-draggable";
-
 const Text =()=>{
   return (
-       <Draggable>
       <div>I am a text asdfsadd</div>
-      </Draggable>
    );
  };
 
